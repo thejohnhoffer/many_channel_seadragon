e2a9d06ae25e8c9dc43fd342edb392200715f8a7
John Hoffer <john@hoff.in>
2018-05-09 08:34:15 -0400

    downgrade build javascript with babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,17 @@ module.exports = {
     context: __dirname,
     mode: 'development',
     entry: ['./src/script.js'],
+    module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: {
+						loader: 'babel-loader',
+					}
+				}
+			]
+    },
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
