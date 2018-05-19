8c1f384c8f38e801713cc2d8c05ab21732fdc458
John Hoffer <john@hoff.in>
2018-05-18 20:48:28 -0400

    drop support for github pages

--- docs/static/vert.glsl
+++
@@ -1,9 +0,0 @@
-#version 300 es
-in vec4 a_pos;
-in vec2 a_tile_pos;
-out vec2 v_tile_pos;
-
-void main() {
-  v_tile_pos = a_tile_pos;
-  gl_Position = a_pos;
-}
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       ]
     },
     output: {
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
     plugins: [
